fd8f337afcc9287f40dc0d2237a875525643309c
ShaunPark <coolage73@gmail.com>
2021-12-07 14:15:11 +0900

    fix

--- delete_ps.sh
+++ delete_ps.sh
@@ -2,8 +2,8 @@
 
 API_SERVER=localhost:5000
 API_URL=http://$API_SERVER/api/v1/users
-
-curl -isb -H "Accept: application/json" $API_URL | jq -c '.[]' | while read object; do
+JQUERY_STR=.data[].id
+curl -isb -H "Accept: application/json" $API_URL | jq -c $JQUERY_STR | while read object; do
   echo $object
 done
 
