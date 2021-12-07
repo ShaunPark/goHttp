93060d021bcc2c84206dac1c291dbb7090c72ac1
ShaunPark <coolage73@gmail.com>
2021-12-07 14:46:44 +0900

    aaa

--- delete_ps.sh
+++ delete_ps.sh
@@ -3,7 +3,7 @@
 API_SERVER=localhost:5000
 API_URL=http://$API_SERVER/api/v1/users
 JQUERY_STR=.data[].id
-PS_DIR
+PS_DIR=/home/ubuntu/testdir
 
 users=()
 curl -sb -H "Accept: application/json" $API_URL | jq -c $JQUERY_STR | while read object; do
@@ -12,7 +12,7 @@ curl -sb -H "Accept: application/json" $API_URL | jq -c $JQUERY_STR | while read
 done
 
 dirs=()
-ls -d ~/*/ | while read dir; do 
+ls -d $PS_DIR/*/ | while read dir; do 
   dirs+=($(echo $dir | awk '{n=split($NF,a,"/");print  a[n-1]}')); 
 done
 
