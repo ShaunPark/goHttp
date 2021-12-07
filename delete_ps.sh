949fe794bc3046570c3b51d5921107fe9db41faf
ShaunPark <coolage73@gmail.com>
2021-12-07 15:27:24 +0900

    aaa

--- delete_ps.sh
+++ delete_ps.sh
@@ -5,8 +5,16 @@ API_URL=http://$API_SERVER/api/v1/users
 JQUERY_STR=.data[].id
 PS_DIR=/home/ubuntu/testdir
 
-users=($(curl -sb -H "Accept: application/json" $API_URL | jq -c $JQUERY_STR | tr -d '"'))
+users=($(curl -sb -H "Accept: application/json" \
+  -H "implicit-authenticated-for: bee.admin" \
+  -H "Authorization: Bearer implicit-token" \
+  $API_URL | jq -c $JQUERY_STR | tr -d '"'))
+
+echo "Total '${#users[@]}' users exist."
+
 dirs=($(ls -d $PS_DIR/*/  | awk '{n=split($NF,a,"/");print  a[n-1]}')); 
+
+echo "Total '${#dirs[@]}' project storage directories exist."
 
 delDirs=()
 for i in "${dirs[@]}"; do
@@ -17,8 +25,15 @@ for i in "${dirs[@]}"; do
     [[ -n $skip ]] || delDirs+=("$i")
 done
 
+echo "Total '${#delDirs[@]}' project storage directories need to be deleted."
 for i in "${delDirs[@]}"
 do
    :
-  echo "rm -rf " $PS_DIR"/"$i
+  DELCMD="rm -rf " $PS_DIR"/"$i
+  read -p "Are you sure delete directory '"$PS_DIR"/"$i"'? (Y/n)" -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+      echo "Deleting "$PS_DIR"/"$i
+  fi
 done
