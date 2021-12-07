8c4aeba61209cb0b82c299b392fb3f32c761943c
ShaunPark <coolage73@gmail.com>
2021-12-07 15:05:52 +0900

    aaa

--- delete_ps.sh
+++ delete_ps.sh
@@ -8,17 +8,18 @@ PS_DIR=/home/ubuntu/testdir
 users=($(curl -sb -H "Accept: application/json" $API_URL | jq -c $JQUERY_STR | tr -d '"'))
 dirs=($(ls -d $PS_DIR/*/  | awk '{n=split($NF,a,"/");print  a[n-1]}')); 
 
-echo "${users[*]}"
-echo "-----------"
-echo "${dirs[*]}"
-echo "-----------"
-for i in "${users[@]}"
-do
-   :
-  echo $i
+delDirs=()
+for i in "${dirs[@]}"; do
+    skip=
+    for j in "${users[@]}"; do
+        [[ $i == $j ]] && { skip=1; break; }
+    done
+    [[ -n $skip ]] || delDirs+=("$i")
 done
+
+echo "${delDirs[*]}"
 echo "-----------"
-for i in "${dirs[@]}"
+for i in "${delDirs[@]}"
 do
    :
   echo $i
