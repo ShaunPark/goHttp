79a0ffa03c8eeb6a48295b10ef341186ec5907ce
ShaunPark <coolage73@gmail.com>
2021-12-07 14:09:41 +0900

    aa

--- delete_ps.sh
+++ delete_ps.sh
@@ -3,7 +3,7 @@
 API_SERVER=localhost:5000
 API_URL=http://$API_SERVER/api/v1/users
 
-$(curl -isb -H "Accept: application/json" $API_URL) | jq -c '.[]' | while read object; do
+curl -isb -H "Accept: application/json" $API_URL | jq -c '.[]' | while read object; do
   echo $object
 done
 
